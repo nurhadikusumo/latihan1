d44387ff85b78e7e7c2615ab3b9e16180a7b4c4c
nurhadikusumo <hadi.gates@gmail.com>
2015-02-26 14:49:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sony/latihan1/model/modelBuku.java
@@ -0,0 +1,30 @@
+package com.example.sony.latihan1.model;
+
+/**
+ * Created by SONY on 2/18/2015.
+ */
+public class modelBuku {
+    private String bookTitle;
+    private String authorName;
+
+   // public modelBuku(String bookTitle, String authorName) {
+     //   this.bookTitle = bookTitle;
+       // this.authorName = authorName;
+    //}
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+}
